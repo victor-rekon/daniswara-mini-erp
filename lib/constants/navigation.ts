6f9efbf9abcb3558d78df698739a90570053a475
victor-rekon <vicczstrife@gmail.com>
2026-06-11 16:28:51 +0700

    Add navigation constants

---
+++ lib/constants/navigation.ts
@@ -0,0 +1,59 @@
+import {
+  BarChart3,
+  ClipboardList,
+  Factory,
+  FileText,
+  LayoutDashboard,
+  ReceiptText,
+  Settings,
+  TableProperties,
+  Truck,
+} from "lucide-react";
+
+export const navigationItems = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "Production / HPP",
+    href: "/production",
+    icon: Factory,
+  },
+  {
+    label: "Quotation",
+    href: "/quotation",
+    icon: FileText,
+  },
+  {
+    label: "Sales & Delivery",
+    href: "/sales-delivery",
+    icon: Truck,
+  },
+  {
+    label: "Invoice & Payment",
+    href: "/invoice-payment",
+    icon: ReceiptText,
+  },
+  {
+    label: "Accounting Light",
+    href: "/accounting",
+    icon: ClipboardList,
+  },
+  {
+    label: "Reports",
+    href: "/reports",
+    icon: BarChart3,
+  },
+  {
+    label: "Master Data",
+    href: "/master-data",
+    icon: TableProperties,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: Settings,
+  },
+];
